867d556ec90e7c9a90e63d248a1908e59ecfed1c
mamedbekovRA <mamedbekovruslan@yandex.ru>
2025-05-10 14:15:57 +0300

    Сделать ограничение перемищения за границы карты

--- main.js
+++ main.js
@@ -1,37 +1,58 @@
 const gameContainer = document.querySelector(".game-container");
+const speed = 10;
 
-const step = 10;
 let x = 0;
 let y = 0;
 
 const player = document.createElement("div");
+
 player.className = "player-object";
-player.style.width = "50px";
-player.style.height = "50px";
+player.style.width = "86px";
+player.style.height = "86px";
 player.style.backgroundColor = "#00a1ff73";
 player.style.position = "relative";
 
 gameContainer.append(player);
 
 document.addEventListener("keydown", (event) => {
+  const available = {
+    top: y,
+    bottom: gameContainer.clientHeight - player.clientHeight - y,
+    left: x,
+    right: gameContainer.clientWidth - player.clientWidth - x,
+  };
+
   switch (event.key) {
     case "ArrowUp":
-      y -= step;
-      player.style.top = `${y}px`;
+      if (available.top > 0) {
+        const move = Math.min(speed, available.top);
+        y -= move;
+        player.style.top = `${y}px`;
+      }
       break;
+
     case "ArrowDown":
-      y += step;
-      player.style.top = `${y}px`;
+      if (available.bottom > 0) {
+        const move = Math.min(speed, available.bottom);
+        y += move;
+        player.style.top = `${y}px`;
+      }
       break;
+
     case "ArrowLeft":
-      x -= step;
-      console.log(x);
-      player.style.left = `${x}px`;
+      if (available.left > 0) {
+        const move = Math.min(speed, available.left);
+        x -= move;
+        player.style.left = `${x}px`;
+      }
       break;
+
     case "ArrowRight":
-      x += step;
-      console.log(x);
-      player.style.left = `${x}px`;
+      if (available.right > 0) {
+        const move = Math.min(speed, available.right);
+        x += move;
+        player.style.left = `${x}px`;
+      }
       break;
 
     default:
